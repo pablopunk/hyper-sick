dc3a09cb94864f39d2fa073120f61e00fa7dc9fc
pablopunk <pablovarela182@gmail.com>
2019-06-06 16:06:36 +0200

    Fix object

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const colors = require('sick-colors')
 
 module.exports.decorateConfig = config => ({
   ...config,
+  colors: {
+    ...colors,
+    lightBlack: colors.black,
+    ligthRed: colors.red,
+    lightGreen: colors.green,
+    lightYellow: colors.yellow,
+    lightBlue: colors.blue,
+    lightMagenta: colors.magenta,
+    lightCyan: colors.cyan
+  },
   backgroundColor: colors.background,
-  foregroundColor: colors.foreground,
-  lightBlack: colors.black,
-  ligthRed: colors.red,
-  lightGreen: colors.green,
-  lightYellow: colors.yellow,
-  lightBlue: colors.blue,
-  lightMagenta: colors.magenta,
-  lightCyan: colors.cyan
+  foregroundColor: colors.foreground
 })
