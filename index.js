2061c4b6262be5de5bd7bf3fc6d83c7fa2a6accf
pablopunk <pablovarela182@gmail.com>
2019-06-06 16:01:41 +0200

    Light again

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ module.exports.decorateConfig = config => ({
   ...config,
   backgroundColor: colors.background,
   foregroundColor: colors.foreground,
-  colors
+  lightBlack: colors.black,
+  ligthRed: colors.red,
+  lightGreen: colors.green,
+  lightYellow: colors.yellow,
+  lightBlue: colors.blue,
+  lightMagenta: colors.magenta,
+  lightCyan: colors.cyan
 })
