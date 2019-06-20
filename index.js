cc25ca2af002c6d19d6459a8ff50a4748b26f7be
pablopunk <pablovarela182@gmail.com>
2019-06-20 16:42:42 +0200

    Use alternative theme as light theme
    *light as in light-colors for terminal

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const colors = require('sick-colors')
 
 module.exports.decorateConfig = config => {
   const themeColors = config.theme === 'light' ? colors.light : colors
+  const altColors = config.theme === 'light' ? colors : colors.light
 
   return {
     ...config,
     colors: {
       ...themeColors,
-      lightBlack: themeColors.black,
-      ligthRed: themeColors.red,
-      lightGreen: themeColors.green,
-      lightYellow: themeColors.yellow,
-      lightBlue: themeColors.blue,
-      lightMagenta: themeColors.magenta,
-      lightCyan: themeColors.cyan
+      lightBlack: altColors.black,
+      ligthRed: altColors.red,
+      lightGreen: altColors.green,
+      lightYellow: altColors.yellow,
+      lightBlue: altColors.blue,
+      lightMagenta: altColors.magenta,
+      lightCyan: altColors.cyan
     },
     backgroundColor: themeColors.background,
     foregroundColor: themeColors.foreground
